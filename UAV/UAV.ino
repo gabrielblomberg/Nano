b718b2b9c8fdce345e4c2c3bf4901b6f41cdcc79
Gabrblal <Gabrblal@users.noreply.github.com>
2019-08-30 16:21:15 +1000

    Added basic logic

--- UAV/UAV.ino
+++ UAV/UAV.ino
@@ -6,5 +6,17 @@ void setup() {}
 // main()
 void loop()
 {
-    Display display();
+    Display display;
+
+    // while (|Acceleration| < 0.5);
+
+    // int start_time = millis();
+    // do stuff
+    // int end_time = millis();
+
+    // int duration = (end_time - start_time)/1000;
+    
+    // display.DisplayInt(duration);
+
+    display.DisplayInt(10);
 }
--- UAV/spike/led.cpp
+++ UAV/spike/led.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+class Test
+{
+    int m_a;
+    public:
+    Test(int a) : m_a(a) {};
+    int Return() {return m_a;}
+};
+
 int main() {
+
+    Test thing(1);
+    std::cout << thing.Return() << std::endl;
 
     bool forwards = true;
     int bits = 5;
