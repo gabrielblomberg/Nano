82d00c9d10a63c209786af3e6c9b4f60a494ef6e
Gabrblal <Gabrblal@users.noreply.github.com>
2019-08-30 15:20:17 +1000

    Small updates

--- UAV/UAV.ino
+++ UAV/UAV.ino
@@ -1,21 +1,10 @@
 #include "Defs.hpp"
 #include "Display.hpp"
-#include "Movement.hpp"
 
-LEDMatrix* p;
+void setup() {}
 
-void init()
-{
-    LEDMatrix p = {
-        LED_PIN_1,
-        LED_PIN_2,
-        LED_PIN_3,
-        LED_PIN_4,
-        LED_PIN_5
-    };
-}
-
+// main()
 void loop()
 {
-
+    Display display();
 }
--- UAV/defs.hpp
+++ UAV/defs.hpp
@@ -3,11 +3,4 @@
 
 typedef int pin;
 
-#define LED_PIN_1 0
-#define LED_PIN_2 1
-#define LED_PIN_3 2
-#define LED_PIN_4 3
-#define LED_PIN_5 4
-#define LED_PIN_6 5
-
 #endif // DEFS_H
